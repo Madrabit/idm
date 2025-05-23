1aa50bd1151be8467ab60e340926a6ea90a23785
Madrabit <madrabot@gmail.com>
2025-05-23 18:55:07 +0300

    add go.mod

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go.")
+	if true {
+		fmt.Printf("Hello, World!")
+	}
+	if err := someFunction(); err != nil {
+		fmt.Println("Error occurred")
+	}
+}
+
+func someFunction() error {
+	return fmt.Errorf("this is an error")
 }
