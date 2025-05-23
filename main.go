29f8a973ed38315d4c4901d2d56797410299d358
Madrabit <madrabot@gmail.com>
2025-05-23 18:57:05 +0300

    fix main

--- main.go
+++ main.go
@@ -3,14 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	if true {
-		fmt.Printf("Hello, World!")
-	}
-	if err := someFunction(); err != nil {
-		fmt.Println("Error occurred")
-	}
-}
-
-func someFunction() error {
-	return fmt.Errorf("this is an error")
+	fmt.Println("Hello, Go.")
 }
