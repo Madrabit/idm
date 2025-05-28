a1734d049a764bdecd55e9147cdf6080b1de100e
Madrabit <madrabot@gmail.com>
2025-05-28 21:42:13 +0300

    Слой Data. Первые тесты [#505420]

---
+++ inner/common/config_test.go
@@ -0,0 +1,56 @@
+package common
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestGetConfig(t *testing.T) {
+	t.Run("empty .env - full ram env", func(t *testing.T) {
+		_ = eachEnvFile(t, "GOOSE_DBSTRING=mock_DBSTRING\nDB_DRIVER_NAME=postgres\nDB_DSN=mock_DSN")
+		t.Setenv("DB_DRIVER_NAME", "postgres")
+		t.Setenv("DB_DSN", "great dsn string")
+		cnf := GetConfig("")
+		assert.Equal(t, "postgres", cnf.DbDriverName)
+		assert.Equal(t, "great dsn string", cnf.Dsn)
+	})
+	t.Run("empty .env and empty environment", func(t *testing.T) {
+		t.Setenv("DB_DRIVER_NAME", "")
+		t.Setenv("DB_DSN", "")
+		file := eachEnvFile(t, "")
+		cnf := GetConfig(file)
+		assert.Equal(t, Config{}, cnf)
+		assert.Empty(t, cnf.DbDriverName)
+		assert.Empty(t, cnf.Dsn)
+	})
+	t.Run("empty .env but full environment", func(t *testing.T) {
+		t.Setenv("DB_DRIVER_NAME", "postgres")
+		t.Setenv("DB_DSN", "great dsn string")
+		file := eachEnvFile(t, "")
+		cnf := GetConfig(file)
+		assert.NotEqual(t, Config{}, cnf, "config not exists")
+		assert.NotEmpty(t, cnf.DbDriverName, "environments not empty")
+		assert.NotEmpty(t, cnf.Dsn, "environments not empty")
+	})
+	t.Run("check env load priority", func(t *testing.T) {
+		t.Setenv("DB_DRIVER_NAME", "ram_env")
+		t.Setenv("DB_DSN", "ram_env")
+		file := eachEnvFile(t, "GOOSE_DBSTRING=mock_DBSTRING\nDB_DRIVER_NAME=postgres\nDB_DSN=mock_DSN")
+		cnf := GetConfig(file)
+		assert.Equal(t, "ram_env", cnf.Dsn, "expected values from environment")
+		assert.Equal(t, "ram_env", cnf.DbDriverName, "expected values from environment")
+	})
+}
+
+func eachEnvFile(t *testing.T, str string) string {
+	t.Helper()
+	temp, err := os.CreateTemp(".", ".env")
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		os.Remove(temp.Name())
+	})
+	temp.WriteString(str)
+	temp.Close()
+	return temp.Name()
+}
--- inner/database/database.go
+++ inner/database/database.go
@@ -6,18 +6,16 @@ import (
 	"time"
 )
 
-var DB *sqlx.DB
-
 func ConnectDb() *sqlx.DB {
 	cfg := common.GetConfig(".env")
 	return ConnectDbWithCfg(cfg)
 }
 
 func ConnectDbWithCfg(cfg common.Config) *sqlx.DB {
-	DB = sqlx.MustConnect(cfg.DbDriverName, cfg.Dsn)
-	DB.SetMaxIdleConns(5)
-	DB.SetMaxOpenConns(20)
-	DB.SetConnMaxLifetime(1 * time.Minute)
-	DB.SetConnMaxIdleTime(10 * time.Minute)
-	return DB
+	db := sqlx.MustConnect(cfg.DbDriverName, cfg.Dsn)
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(1 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+	return db
 }
