c58882a758ad275f56b490f29caf9a8c20a5b006
Madrabit <madrabot@gmail.com>
2025-06-05 11:41:26 +0300

    Слой Service. Тесты бизнес-логики. Mock и stub объекты [#505423] fix: убрана обработка ошибок из репозитория и варнинги поправил

--- inner/role/repository.go
+++ inner/role/repository.go
@@ -1,42 +1,44 @@
 package Role
 
 import (
-	"database/sql"
-	"errors"
-	"fmt"
 	"github.com/jmoiron/sqlx"
 	"time"
 )
 
-type RoleRepository struct {
+type Repository struct {
 	db *sqlx.DB
 }
 
-func NewRoleRepository(db *sqlx.DB) *RoleRepository {
-	return &RoleRepository{db: db}
+func NewRoleRepository(db *sqlx.DB) *Repository {
+	return &Repository{db: db}
 }
 
-type RoleEntity struct {
+type Entity struct {
 	Id       int64     `db:"id"`
 	Name     string    `db:"name"`
 	CreateAt time.Time `db:"created_at"`
 	UpdateAt time.Time `db:"updated_at"`
 }
 
-func (r *RoleRepository) FindById(id int64) (role RoleEntity, err error) {
+func (r *Repository) FindById(id int64) (role Entity, err error) {
 	err = r.db.Get(&role, "SELECT * FROM role WHERE id=$1", id)
 	return role, err
 }
 
-func (r *RoleRepository) GetAll() ([]RoleEntity, error) {
-	var roles []RoleEntity
+func (r *Repository) GetAll() ([]Entity, error) {
+	var roles []Entity
 	rows, err := r.db.Queryx("SELECT * FROM role")
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			return
+		}
+	}()
 	for rows.Next() {
-		var role RoleEntity
+		var role Entity
 		if err = rows.StructScan(&role); err != nil {
 			return nil, err
 		}
@@ -45,7 +47,7 @@ func (r *RoleRepository) GetAll() ([]RoleEntity, error) {
 	return roles, nil
 }
 
-func (r *RoleRepository) Add(role RoleEntity) (int64, error) {
+func (r *Repository) Add(role Entity) (int64, error) {
 	var id int64
 	err := r.db.QueryRow("INSERT INTO Role (name) VALUES ($1) RETURNING id",
 		role.Name).Scan(&id)
@@ -55,10 +57,7 @@ func (r *RoleRepository) Add(role RoleEntity) (int64, error) {
 	return id, nil
 }
 
-func (r *RoleRepository) GetGroupById(ids []int64) (roles []RoleEntity, err error) {
-	if len(ids) == 0 {
-		return nil, errors.New("role id can not be empty")
-	}
+func (r *Repository) GetGroupById(ids []int64) (roles []Entity, err error) {
 	q, args, err := sqlx.In("SELECT * FROM role WHERE id IN (?)", ids)
 	if err != nil {
 		return nil, err
@@ -71,25 +70,15 @@ func (r *RoleRepository) GetGroupById(ids []int64) (roles []RoleEntity, err erro
 	return roles, nil
 }
 
-func (r *RoleRepository) Delete(id int64) (err error) {
-	e, err := r.FindById(id)
+func (r *Repository) Delete(id int64) (err error) {
+	_, err = r.db.Exec("DELETE FROM role WHERE id=$1", id)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return fmt.Errorf("role not found: %d", id)
-		}
-		return fmt.Errorf("db error %w", err)
-	}
-	_, err = r.db.Exec("DELETE FROM role WHERE id=$1", e.Id)
-	if err != nil {
-		return fmt.Errorf("failed to delete role: %w", err)
+		return err
 	}
 	return nil
 }
 
-func (r *RoleRepository) DeleteGroup(ids []int64) error {
-	if len(ids) == 0 {
-		return nil
-	}
+func (r *Repository) DeleteGroup(ids []int64) error {
 	q, args, err := sqlx.In("DELETE FROM role WHERE id IN (?)", ids)
 	if err != nil {
 		return err
