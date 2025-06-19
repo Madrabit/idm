0bdad50d41798f218033d652ac80871fe12a7092
Madrabit <madrabot@gmail.com>
2025-06-19 12:48:05 +0300

    Слой API. Добавляем внутренний API [#505438]

--- inner/common/config.go
+++ inner/common/config.go
@@ -1,6 +1,9 @@
 package common
 
 import (
+	"errors"
+	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 	"os"
 )
@@ -8,13 +11,27 @@ import (
 type Config struct {
 	DbDriverName string `validate:"required"`
 	Dsn          string `validate:"required"`
+	AppName      string `validate:"required"`
+	AppVersion   string `validate:"required"`
 }
 
 func GetConfig(envFile string) Config {
-	_ = godotenv.Load(envFile)
+	err := godotenv.Load(envFile)
+	if err != nil {
+		fmt.Printf("error loading .env file: %v", err)
+	}
 	var cfg = Config{
 		DbDriverName: os.Getenv("DB_DRIVER_NAME"),
 		Dsn:          os.Getenv("DB_DSN"),
+		AppName:      os.Getenv("APP_NAME"),
+		AppVersion:   os.Getenv("APP_VERSION"),
+	}
+	err = validator.New().Struct(cfg)
+	if err != nil {
+		var validateErrs validator.ValidationErrors
+		if errors.As(err, &validateErrs) {
+			panic(fmt.Sprintf("config validation error: %v", err))
+		}
 	}
 	return cfg
 }
